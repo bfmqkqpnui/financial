cf11d8c04a36c62d1fb82403d8830e49efa62684
bfmqkqpnui <47877962@qq.com>
2018-12-14 14:42:27 +0800

    更新--优化

--- src/const.js
+++ src/const.js
@@ -1,10 +1,12 @@
 let u = navigator.userAgent;
 let isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; // android终端
 let isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); // ios终端
-
-const SERVICE_BASE_URL = '/apiPath'
+// 代理路径全局变量
+let SERVICE_BASE_URL = '/apiPath'
 const PLATFORM = isiOS ? 'iOS' : isAndroid ? 'Android' : ''
-
+if (process.env.NODE_ENV === 'production') {
+  SERVICE_BASE_URL = '/apiPath'
+}
 
 export default {
   PLATFORM, // 移动终端
